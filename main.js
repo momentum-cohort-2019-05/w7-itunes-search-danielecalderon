1291472c0f9d81460c476c19c63e2bc16334abba
Daniel E Calderon <Daniel.Calderon06@gmail.com>
2019-07-12 11:57:56 -0400

    inner html

--- main.js
+++ main.js
@@ -10,15 +10,17 @@ document.addEventListener('click', (e) => {
 
 function getData() {
 
-    fetch('https://itunes-api-proxy.glitch.me/search?term=linkin+park')
+   url = 'https://itunes-api-proxy.glitch.me/search?term=linkin+park'
+
+    fetch(url)
     .then(data => data.json())
     .then(json => {
-        console.log(json)
+      console.log(json)
         let finalHTML = ''
 
         json.results.forEach( song => {
             finalHTML += `
-            <div> class ="col s4 m4 l4">
+            <div class ="col s4 m4 l4"> 
             <div class="card" style="width: 18rem;">
             <img src="${song.artworkUrl60}" 
             class="card-img-top" alt="...">
@@ -26,6 +28,13 @@ function getData() {
               <h5 class="card-title">${song.artistName}</h5>
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
               <a href="#" class="btn btn-primary">Listen</a>
+              <figure>
+              <figcaption>Listen to the T-Rex:</figcaption>
+              <audio controls src="/media/examples/t-rex-roar.mp3">
+            Your browser does not support the
+            <code>audio</code> element.
+    </audio>
+</figure>
             </div>
           </div>
           </div>
